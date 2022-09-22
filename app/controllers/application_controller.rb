9ede15eb059ebaa62d33ebeecd3a84539a892db1
Valov <m.valov@trucker.group>
2022-09-21 21:04:11 -0300

    [dry] Errors standard Not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include Responses
+
+  rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound do
+    render not_found_error
+  end
 end
